ef6d8974a5e986485fb2f2b6aba17241c473d01f
Ivaylo Donchev <idonchev@hotmail.com>
2017-10-23 11:26:21 +0300

    dfgv

--- Pointers/Pointers/Source.cpp
+++ Pointers/Pointers/Source.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>	//for_each
 using namespace std;
 
 int main()
 {
+	int mas[10]{ 1,2,3,4,5 };
+	
+	for (size_t i = 0; i < sizeof(mas)/sizeof(mas[0]); i++)
+		cout << mas[i] << " ";
+	cout << endl;
+	
+	for (auto x : mas)
+		cout << x << " ";
+	cout << endl;
 
+	for (auto i = begin(mas); i != end(mas); ++i)
+		cout << *i << " ";
+	cout << endl;
+
+	for_each(begin(mas), end(mas), [](auto x) {
+		cout << x << " "; });
+	cout << endl;
+
+	system("pause");
 }
