51262cbdacf880a75194f3a4cea12bb135151a48
Ivaylo Donchev <idonchev@hotmail.com>
2017-10-23 14:49:07 +0300

    ертф

--- Pointers/Pointers/Source.cpp
+++ Pointers/Pointers/Source.cpp
@@ -6,24 +6,29 @@ using namespace std;
 int main()
 {
 	int mas[]{ 1,2,3,4,5 };
-	size_t n{ sizeof(mas) / sizeof(mas[0]) };
+	size_t n{ sizeof(mas) / sizeof(mas[0]) };	//брой на елементите на масива
 	
+	//обхождане чрез индекс
 	for (size_t i = 0; i < n; i++)
 		cout << mas[i] << " ";
 	cout << endl;
 	
+	//обхождане с range-based оператор за цикъл
 	for (auto x : mas)
 		cout << x << " ";
 	cout << endl;
 
+	//обхождане с итератор
 	for (auto i = begin(mas); i != end(mas); ++i)
 		cout << *i << " ";
 	cout << endl;
 
+	//обхождане с алгоритъма for_each и обработка на всеки елемент с ламбда
 	for_each(begin(mas), end(mas), [](auto x) {
 		cout << x << " "; });
 	cout << endl;
 
+	//същото, но с директно задаване на началото и края
 	for_each(mas, mas+n, [](auto x) {
 		cout << x << " "; });
 	cout << endl;
