92fb6ca50af443384479f1b66a20b317a4ea70b7
Ivaylo Donchev <idonchev@hotmail.com>
2017-11-29 14:33:16 +0200

    wdf

--- Lect_0711/Lect_0711/Source.cpp
+++ Lect_0711/Lect_0711/Source.cpp
@@ -13,106 +13,130 @@ public:
 
 int main()
 {
-	/*int* p = new int[5];
+	/*int *p;
+	p = new int;
+	*p = 10;
+	int *q = new int(20);
+	int n;
+	cout << "n = "; cin >> n;
+	double *dp = new double[n];
+	dp[0] = 2.34;
+	dp[1] = dp[0] * 2 + 1;*/
+
+
+	/*int* p{ new int[5] };
 	p[4] = 7;
 	p[6] = 10;
-	p[-3] = 15;*/
+	p[-3] = 15;
+
+	int *p1 = new int[500];
+	int *p2 = new int;
+	p1[200] = 10;
+	p1 = p2;
+	int x = p1[200];
+	cout << "x = " << x << endl;
+	double *p=nullptr;  
+	if(p)
+		*p = 3.5;
+	cout << *p << endl;
+*/
+
 //--------------------------------------------------------------------------
-	/*double *p{ new double(2.5) };
-	cout << p << ", " << *p << endl;
-	delete p;
-	cout << p << endl;
-	cout << *p << endl;	//null pointer dereferencing*/
+	//double *p{ new double(2.5) };
+	//cout << p << ", " << *p << endl;
+	//delete p;
+	//cout << p << endl;
+	//cout << *p << endl;	//null pointer dereferencing
 //--------------------------------------------------------------------------
-	//използване на void* за достъп до различни типове обекти
-/*	void* vp;
-	vp = new double;
-	cout << "Enter double: ";
-	cin >> *static_cast<double*>(vp);
-	cout << *(double*)vp << endl;
-	delete (double*) vp;
+	////използване на void* за достъп до различни типове обекти
+	//void* vp;
+	//vp = new double;
+	//cout << "Enter double: ";
+	//cin >> *static_cast<double*>(vp);
+	//cout << *(double*)vp << endl;
+	//delete (double*) vp;
 
-	vp = new MyClass;
-	// delete vp;	// Неправилно освобождаване на паметта
-	delete (MyClass*)vp;	// Коректно - ще се изпълни деструкторът на MyClass
+	//vp = new MyClass;
+	//// delete vp;	// Неправилно освобождаване на паметта
+	//delete (MyClass*)vp;	// Коректно - ще се изпълни деструкторът на MyClass
 
-	vp = new int[5];
-	cout << "Enter 5 integers: ";
-	for (int i = 0; i < 5; i++)
-		cin >> *(static_cast<int*>(vp) + i);
-	for (int i = 0; i < 5; i++)
-		cout << static_cast<int*>(vp)[i] << ' ';
-	cout << endl;
-	delete [] (int*)vp;
+	//vp = new int[5];
+	//cout << "Enter 5 integers: ";
+	//for (int i = 0; i < 5; i++)
+	//	cin >> *(static_cast<int*>(vp) + i);
+	//for (int i = 0; i < 5; i++)
+	//	cout << static_cast<int*>(vp)[i] << ' ';
+	//cout << endl;
+	//delete [] (int*)vp;
 
-	vp = new MyClass[10];
-	//delete (MyClass*) vp;	// ще освободи паметта само за първия обект (и може да забие)
-	delete[](MyClass*) vp;	// коректно - ще освободи паметта за 10 обекта
-	MyClass* mp = new MyClass[5]; // същото чрез указател към класа
-	cout << "--------------- MyClass pointer ---------------\n";
-	delete[] mp;
-	*/
+	//vp = new MyClass[10];
+	////delete (MyClass*) vp;	// ще освободи паметта само за първия обект (и може да забие)
+	//delete[](MyClass*) vp;	// коректно - ще освободи паметта за 10 обекта
+	//MyClass* mp = new MyClass[5]; // същото чрез указател към класа
+	//cout << "--------------- MyClass pointer ---------------\n";
+	//delete[] mp;
+
 //-----------------------------------------------------------------------
-/*	// Псевдоними
-	int x = 10, y = 100;
-	int &rx = x;
-	cout << "x =  " << x << endl;
-	x = 30;
-	cout << "rx = " << rx << endl;
-	//адресите на двете променливи съвпадат
-	cout << "&rx = " << &rx << endl;
-	cout << "&x  = " << &x << endl;
+	//// Псевдоними
+	//int x = 10, y = 100;
+	//int &rx = x;
+	//cout << "x =  " << x << endl;
+	//x = 30;
+	//cout << "rx = " << rx << endl;
+	////адресите на двете променливи съвпадат
+	//cout << "&rx = " << &rx << endl;
+	//cout << "&x  = " << &x << endl;
 
-	//rx = &y;	// грешка - псведоним не може да се свърже с друга променлива
-	//int &ry;    // грешка - псевдонимът не е инициализиран
-	//reinterpret_cast<int*>(&rx) = &y; // преди можеше така
-	const int& cr = 25;	//псевдоним на числов литерал
-	//cr = 30;	// грешка (псевдонимът е константен)
-	const_cast<int&>(cr) = 30;  // така става, но има по-елегантен начин
-	cout << "cr = " << cr << endl;
+	////rx = &y;	// грешка - псведоним не може да се свърже с друга променлива
+	////int &ry;    // грешка - псевдонимът не е инициализиран
+	////reinterpret_cast<int*>(&rx) = &y; // преди можеше така
+	//const int& cr = 25;	//псевдоним на числов литерал
+	////cr = 30;	// грешка (псевдонимът е константен)
+	//const_cast<int&>(cr) = 30;  // така става, но има по-елегантен начин
+	//cout << "cr = " << cr << endl;
 
-	//десни псевдоними
-	int && rr = 20;	//десен псевдоним на числовия литерал;
-	cout << "rr = " << rr << endl;
-	cout << "&rr = " << &rr << endl;
-	rr = 30;
-	cout << "rr = " << rr << endl;
-	cout << "&rr = " << &rr << endl; //на същото място, където преди това беше литералът 20
-	*/
+	////десни псевдоними
+	//int && rr = 20;	//десен псевдоним на числовия литерал;
+	//cout << "rr = " << rr << endl;
+	//cout << "&rr = " << &rr << endl;
+	//rr = 30;
+	//cout << "rr = " << rr << endl;
+	//cout << "&rr = " << &rr << endl; //на същото място, където преди това беше литералът 20
+	
 //.................................................................................
-/*	//Пример: Динамичен масив с реалокация
-	int *p{ new int[5] };
-	int *q{ nullptr };
-	int x{ 0 }, n{ 0 }, m{ 0 };
-	//въвеждане на числа, докато се въведе 0
-	cout << ">> "; cin >> x;
-	while (x != 0)
-	{
-		if (m <= 5)
-		{
-			m = 0;
-			q = new int[n + 5];
-			memmove(q, p, n * sizeof(int));
-			delete[] p;
-			p = q;
-		}
-		p[n++] = x;
-		++m;
-		cout << ">> "; cin >> x;
-	}
-	//извеждане на числата
-	for (int i = 0; i < n; i++)
-		cout << p[i] << ' ';
-	cout << endl;
-	delete[] p;
-	*/
+	////Пример: Динамичен масив с реалокация
+	//int *p{ new int[5] };
+	//int *q{ nullptr };
+	//int x{ 0 }, n{ 0 }, m{ 0 };
+	////въвеждане на числа, докато се въведе 0
+	//cout << ">> "; cin >> x;
+	//while (x != 0)
+	//{
+	//	if (m >= 5)
+	//	{
+	//		m = 0;
+	//		q = new int[n + 5];
+	//		memmove(q, p, n * sizeof(int));
+	//		delete[] p;
+	//		p = q;
+	//	}
+	//	p[n++] = x;
+	//	++m;
+	//	cout << ">> "; cin >> x;
+	//}
+	////извеждане на числата
+	//for (int i = 0; i < n; i++)
+	//	cout << p[i] << ' ';
+	//cout << endl;
+	//delete[] p;
+	//
 //----------------------------------------------------------------------------
-/*	//двумерен масив чрез двоен указател
+	//двумерен масив чрез двоен указател
 	int m, n;
 	int **matr;
 	cout << "rows: "; cin >> m;
 	cout << "columns: "; cin >> n;
-	//Заделяна на памет
+	//Заделяне на памет
 	matr = new int*[m];
 	for (int i = 0; i < m; i++)
 		matr[i] = new int[n];
@@ -120,7 +144,7 @@ int main()
 	srand((unsigned)time(0));
 	for (int i = 0; i < m; i++)
 		for (int j = 0; j < n; j++)
-			matr[i][j] = rand() % 10;
+			matr[i][j] = rand() % 10; 
 	//Извеждане във вид на матрица
 	for (int i = 0; i < m; i++)
 	{
@@ -142,7 +166,6 @@ int main()
 	for (int i = 0; i < m; i++)
 		delete [] matr[i];
 	delete [] matr;
-	*/
 
 	system("pause");
 }
--- Work/Work/Source.cpp
+++ Work/Work/Source.cpp
@@ -1,8 +1,159 @@
 #include <iostream>
+#include <iomanip>
 #include <algorithm>
+#include <ctime>
 using namespace std;
 
 int main()
 {
+	//const int n = 10; //брой на оценките
+	//double list[n];   //масив, който ще съдържа оценките
+	//for (int i = 0; i < n; i++)
+	//{
+	//	do {
+	//		cout << "Evaluation for student # " << i + 1 << ": ";
+	//		cin >> list[i];
+	//	} while (list[i]<2.0 || list[i]>6.0);
 
+	//}
+	//cout << endl << "---- Results ----\n";
+	//cout.setf(ios::fixed | ios::showpoint);
+	//cout.precision(2);
+	//for (int i = 0; i < n; i++)
+	//	cout << "Student # " << i + 1 << '\t'
+	//	<< " evaluation: " << list[i] << endl;
+	//cout << endl;
+	//
+	//int i = 20;
+	//int *pi = &i;
+	//cout << "*pi = " << *pi << endl;
+	//*pi = 50;
+	//cout << "i = " << i << endl;
+	//i = 100;
+	//cout << "*pi = " << *pi << endl;
+	//cout << "pi = " << pi << endl;
+	//cout << "&i = " << &i << endl;
+
+	//char ch = 'A';
+	//char* chp = &ch;
+	//pi = reinterpret_cast<int*>(chp);
+
+
+	//pi = reinterpret_cast<int*>(0x12FF60);  //OK, но не е добре да се прави
+
+	//// Операция sizeof
+	//cout << "size of char = " << sizeof(char) << endl;
+	//cout << "size of int = " << sizeof(int) << endl;
+	//cout << "size of double = " << sizeof(double) << endl;
+
+	//cout << "sizeof 'A' = " << sizeof('A') << endl;
+	//cout << "sizeof (3+20) = " << sizeof(3 + 20) << endl;
+	//cout << "sizeof (3*2.5) = " << sizeof(3 * 2.5) << endl;
+	//cout << "sizeof pi = " << sizeof(pi) << endl;
+	//cout << "sizeof chp = " << sizeof(chp) << endl;
+
+	/*char s[] = "Ivan Ivanov";
+	char *p = s;
+	char *q = s + 5;
+	cout << "p = " << p << endl;
+	cout << "q = " << q << endl;
+	cout << "q-p = " << q - p << endl;*/
+
+	/*int mas[]{ 5,10,15,20,25,30,35,40,45,50 };
+	int *p1, *p2;
+	p1 = &mas[3];
+	p2 = &mas[7];
+	cout << "p1 = " << p1 << '\t' << "*p1 = " << *p1 << endl;
+	cout << "p2 = " << p2 << '\t' << "*p2 = " << *p2 << endl;
+	cout << "*(p1+1) = " << *(p1 + 1) << endl;
+	cout << "*(p1-1) = " << *(p1 - 1) << endl;
+	cout << "p2-p1 = " << p2 - p1 << endl;
+	p2++;
+	cout << "p2 = " << p2 << '\t' << "*p2 = " << *p2 << endl;
+	p1 -= 2;
+	cout << "p1 = " << p1 << '\t' << "*p1 = " << *p1 << endl;
+*/
+
+	//int i = 25;
+	//double d = 2.5;
+	//void *p;
+	//p = &i;	// p сочи данни от тип int
+	//cout << *(int*)p << endl;
+	//p = &d; // p сочи данни от тип double
+	//cout << *static_cast<double*>(p) << endl;
+
+	//int mas[5] = { 6,3,5,1,2 };
+	//cout << mas[0] << endl;	    // 6
+	//cout << *mas << endl;		// 6
+	//cout << mas[1] << endl;		// 3
+	//cout << *(mas + 1) << endl;	// 3
+	//int *p;
+	//p = mas;
+	//cout << *(p + 2) << endl;	// 5
+	//cout << p[2] << endl;		// 5
+
+	/*int *p;
+	p = new int;
+	*p = 10;
+	int *q = new int(20);
+	int n;
+	cout << "n = "; cin >> n;
+	double *dp = new double[n];
+	*dp = 2.5;
+	*(dp + 1) = 3.9;
+	dp[2] = 4.0;
+	double x = dp[1];
+	for (int i = 0; i < n; i++)
+		cout << dp[i];
+*/
+
+//
+//double *p=nullptr; //0 или NULL
+//
+//p = new double;
+//if(p!=nullptr){}
+//if(p)
+//
+//*p = 3.5;
+//cout << *p << endl;
+//delete p;	//освобождава заделената с new памет
+
+
+void *vp;
+vp = new double;
+cout << "Enter double number: ";
+cin >> *static_cast<double*>(vp);
+delete (double*) vp;
+vp = new int[5];
+cout << "Enter 5 integers: ";
+for (int i = 0; i < 5; i++)
+	cin >> *(static_cast<int*>(vp) + i);
+for (int i = 0; i < 5; i++)
+	cout << static_cast<int*>(vp)[i] << ' ';
+cout << endl;
+delete[](double*)vp;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	system("pause");
 }
